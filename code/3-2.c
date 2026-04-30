334ac9143d84213e6c967de818ecfc28571bcaee
H1yeee19 <happylacu@gmail.com>
2026-05-01 00:13:22 +0900

    Update 3-2.c

--- code/3-2.c
+++ code/3-2.c
@@ -1,26 +1,25 @@
 #include <stdio.h>
 #include <string.h>
 
-void bubbleSort(char *arr[], int n) {    // 문자열 배열을 버블 정렬하는 함수
-        for (int i = 0; i < n-1; i++) {     //
-            for (int j = 0; j < n-i-1; j++) {
-                if (strcmp(arr[j], arr[j+1]) > 0) {
-                    char *temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
-                }
+int main(int argc, char *argv[]) {
+    // 버블 정렬 알고리즘
+    // argv[0]은 프로그램의 실행 경로/ 이름이므로 정렬에서 제외하고 인덱스 1부터 실행
+    for (int i = 1; i < argc - 1; i++) {
+        for (int j = 1; j < argc - i; j++) {
+            // strcmp 함수는 두 문자열을 비교하여 사전적으로 어느 것이 더 큰지 판단
+            if (strcmp(argv[j], argv[j + 1]) > 0) {
+                // 문자열 비교 결과가 양수이면 argv[j]가 argv[j + 1]보다 사전적으로 뒤에 위치
+                // 포인터를 교환하여 문자열의 위치를 바꿔줌.
+                char *temp = argv[j];
+                argv[j] = argv[j + 1];
+                argv[j + 1] = temp;
             }
         }
     }
 
-int main(int argc, char *argv[]) {  //argc: argument count, argv: argument variable
-    //char *argv[]: 문자열의 주소를 저장하는 포인터 배열, 각 인자는 띄어쓰기로 구분됨.
-    // char *argv[] = {"./a.out", "apple", "mango","ant", "banana", "cat", "anaconda"};
-    // char *argv[] == char **argv (char 포인터의 배열 == char 포인터에 대한 포인터)
-
-    bubbleSort(argv + 1, argc - 1);  // argv[0]은 프로그램 이름이므로 제외
-
+    // 정렬된 문자열을 출력
     for (int i = 1; i < argc; i++) {
-        printf("%s\n ", argv[i]);
+        printf("%s\n", argv[i]);
     }
+    return 0;
 }
